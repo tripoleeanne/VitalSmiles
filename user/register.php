99212da2c815ce3bfa54b0b6c54662f1bcba1c1b
Leanne Tripoli <leanne.tripoli123@gmail.com>
2023-01-07 20:16:05 +0800

    added database

--- user/register.php
+++ user/register.php
@@ -1,4 +1,42 @@
+<?php 
+require('./database.php');
 
+if(isset($_POST['register']))
+{
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+    $confirmPassword = trim($_POST['confirmPassword']);
+
+    if(empty($username) || empty($password) || empty($confirmPassword)){
+       $errorMessage = "Please fill all the fields";
+    }
+    else{
+        if($password != $confirmPassword){
+           $errorMessage = "Password does not match.";
+        }
+        else{
+            $queryValidate = "SELECT username FROM users WHERE username = '$username'";
+            $sqlValidate = mysqli_query($connection, $queryValidate);
+
+            if(mysqli_num_rows($sqlValidate) > 0){
+                $errorMessage = "Username already exists.";
+            }
+            
+            else{
+                $queryRegister = "INSERT INTO users VALUES (NULL,NULL,$username,md5($password),null,null,null,null,null)";
+                $sqlRegister = mysqli_query($connection, $queryRegister);
+
+                if($sqlRegister){
+                    $accountCreated = "Account created successfully.";
+                }
+                else{
+                   $errorMessage ="Something went wrong.";
+                }
+            }
+        }
+    }
+}
+?>
 
 <!doctype html>
 <html lang="en">
@@ -6,7 +44,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
+  <title>VitalSmiles</title>
   <style>
     @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
     *{
@@ -27,36 +65,46 @@
     <div class="container py-5 h-75">
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-          <div class="card shadow-2-strong" style="border-radius: 1rem;">
+        <form action="register.php" method="post">
+        <div class="card shadow-2-strong" style="border-radius: 1rem;">
             <div class="card-body p-5 text-center">
               <img src="../assets/image.png" alt="logo" class="img-fluid" style="height:190px;">
               <h4 class="mb-5">Create an Account</h4>
 
               <div class="form-outline mb-4">
-                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX-2">First Name</label>
+                <input type="text" name="username" class="form-control form-control-lg" />
+                <label class="form-label">Username</label>
               </div>
 
               <div class="form-outline mb-4">
-                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                <label class="form-label" for="typeEmailX-2" >Last Name</label>
-              </div>
-
-              <div class="form-outline mb-4">
-                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                <label class="form-label" for="typePasswordX-2">Password</label>
+                <input type="password" name="password" class="form-control form-control-lg" />
+                <label class="form-label">Password</label>
               </div>
               
               <div class="form-outline mb-4">
-                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                <label class="form-label" for="typePasswordX-2">Confirm Password</label>
+                <input type="password" name="confirmPassword" class="form-control form-control-lg" />
+                <label class="form-label">Confirm Password</label>
               </div>
-                          
-              <button class="btn btn-primary btn-lg btn-block mb-3" type="submit" style="background-color:#2C64C6; color:white;">Sign Up</button>
+
+              <?php
+                        if(isset($errorMessage)){
+                        echo '<div class="alert alert-danger" role="alert">'.$errorMessage.'</div>';
+                        } 
+                        else{
+                            if(isset($accountCreated)){
+                                echo '<div class="alert alert-success" role="alert">'.$accountCreated.'</div>';
+                            }
+                        }
+                ?>   
+
+              <input class="btn btn-primary btn-lg btn-block mb-3" name='register' type="submit" style="background-color:#2C64C6; color:white;" value="Register">
              <h6>Already have an account? <a href="./login.php">Log in</a>.</h6>
 
             </div>
           </div>
+
+        </form>
+         
         </div>
       </div>
     </div>
